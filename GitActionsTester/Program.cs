62c3c593d7f6c30e3e61211c87eed1e30e921fa5
Sahendran-Kandasami11 <ST10242607@vcconnect.edu.za>
2024-09-09 10:19:46 +0200

    First Push

---
+++ GitActionsTester/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitActionsTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
